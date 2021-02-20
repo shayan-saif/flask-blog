d6b54f2aaadb575e889249d196035709aaadcf41
root <root@ip-172-31-87-184.ec2.internal>
2021-02-20 19:48:10 +0000

    Current deployment files

--- modules/config.py
+++ modules/config.py
@@ -1,15 +1,18 @@
 import os
+import json
+
+
+with open('/etc/config.json') as config_file:
+	config = json.load(config_file)
 
 
 class Config:
-    # SECRET_KEY = 'd0bc30efaf760b0a37c1720de62dd95f'
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = config.get('EMAIL_USER')
+    MAIL_PASSWORD = config.get('EMAIL_PASS')
 
  
